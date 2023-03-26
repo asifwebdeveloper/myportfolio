424d321f13935e2b354322fab976c8f5c5d23868
asifwebdeveloper <128634807+asifwebdeveloper@users.noreply.github.com>
2023-03-27 01:44:44 +0300

    Navbar Completed

---
+++ src/components/Layouts/index.jsx
@@ -0,0 +1,13 @@
+import '../Layouts/index.scss'
+import React from 'react'
+import Sidebar from './Sidebar'
+
+const index = () => {
+  return (
+    <>
+    <Sidebar/>
+    </>
+  )
+}
+
+export default index
